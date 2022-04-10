fb9866c59aa55cc8f1dd383d2901f9bb048f5d20
Thato Ramalepa <tramalepa@my.wctc.edu>
2022-04-09 20:49:20 -0500

    Add project files.

---
+++ BuyAlcoholApp/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BuyAlcoholApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            myDatePicker.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var CurrentYear = DateTime.Now.Year;
+            var DOB_Year = myDatePicker.Date.Year;
+
+            var Age = CurrentYear - DOB_Year;
+
+            if (Age < 21)
+            {
+                DisplayAlert("Message", $"You're {Age}, You are underage!!!", "Ok");
+            } 
+            else if (Age > 21)
+            {
+                DisplayAlert("Message", $"You're {Age}, You are safe!!!", "Ok");
+            }
+           
+
+        }
+
+        void OnDateSelected(object sender, DateChangedEventArgs args)
+        {
+            Recalculate();
+        }
+
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            Recalculate();
+        }
+
+        void Recalculate()
+        {
+            TimeSpan timeSpan = endDatePicker.Date - startDatePicker.Date +
+                (includeSwitch.IsToggled ? TimeSpan.FromDays(1) : TimeSpan.Zero);
+
+            resultLabel.Text = String.Format("{0} day{1} between dates",
+                                             timeSpan.Days, timeSpan.Days == 1 ? "" : "s");
+        }
+    }
+}
